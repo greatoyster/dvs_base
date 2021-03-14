b92924b9018b4e000f9359669fff5fc31d66f86d
greatoyster <yangjq@shanghaitech.edu.cn>
2021-03-14 23:31:56 +0800

    add undistortion function

--- include/dvs_base/dvs_base_camera.hpp
+++ include/dvs_base/dvs_base_camera.hpp
@@ -4,6 +4,7 @@
 #include <image_geometry/pinhole_camera_model.h>
 #include <opencv2/opencv.hpp>
 #include <Eigen/Eigen>
+#include <glog/logging.h>
 
 namespace dvs_base
 {
@@ -13,17 +14,19 @@ namespace dvs_base
 
         auto height = cam.fullResolution().height;
         auto width = cam.fullResolution().width;
-        Eigen::Matrix<Scalar, 2, -1> map(2, height * width);
+        Eigen::Matrix<Scalar, 2, -1> lookup_table(2, height * width);
+
         for (auto h = 0; h < height; ++h)
         {
             for (auto w = 0; w < width; ++w)
             {
-                cv::Point2d p = cam.rectifyPoint(cv::Point2d(h, w));
-                map(h * width + w, 0) = Scalar(p.x);
-                map(h * width + w, 1) = Scalar(p.y);
+                cv::Point2d rectified_point = cam.rectifyPoint(cv::Point2d(h, w));
+                lookup_table(0, h * width + w) = Scalar(rectified_point.x);
+                lookup_table(1, h * width + w) = Scalar(rectified_point.y);
             }
         }
-        return map;
+        
+        return lookup_table;
     }
 }
 #endif
--- include/dvs_base/dvs_base_exp_sys.hpp
+++ include/dvs_base/dvs_base_exp_sys.hpp
@@ -3,10 +3,31 @@
 
 #include <boost/format.hpp>
 #include <boost/filesystem.hpp>
+#include <chrono>
+#include <Eigen/Eigen>
 #include <glog/logging.h>
+#include <random>
 
 namespace dvs_base
 {
+    namespace utils
+    {
+        template <typename Scalar>
+        Eigen::Matrix<Scalar, -1, 1> getNormalDistribution(Scalar mean, Scalar scale, size_t n)
+        {
+            Eigen::Matrix<Scalar, -1, 1> dist_vec(n, 1);
+
+            unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+            std::default_random_engine gen(seed);
+            std::normal_distribution<Scalar> dis(mean, scale);
+
+            for (auto i = 0; i < n; ++i)
+                dist_vec[i] = dis(gen);
+
+            return dist_vec;
+        }
+
+    }
     namespace exp_sys
     {
         static boost::filesystem::path root_dir_;
@@ -20,21 +41,22 @@ namespace dvs_base
             boost::filesystem::path exps_path = root_dir_;
             exps_path.append("exps");
             LOG(INFO) << root_dir_.string();
+
             if (!exists(exps_path))
-            {
                 create_directory(exps_path);
-            }
+
             directory_iterator end;
             directory_iterator pos(exps_path);
             int max_id = 0;
+
             for (; pos != end; pos++)
             {
                 int exp_id = std::atoi(pos->path().filename().c_str());
                 if (exp_id >= max_id)
                     max_id = exp_id;
             }
-            max_id++;
-            path exp_path = exps_path.append((boost::format("%03d") % max_id).str());
+
+            path exp_path = exps_path.append((boost::format("%03d") % (max_id + 1)).str());
             create_directory(exp_path);
             output_dir_ = exp_path;
         }
@@ -53,18 +75,21 @@ namespace dvs_base
         {
             auto ret = root_dir_;
             ret.append(relative_path);
+
             return ret;
         }
 
-        std::string filenameBySeq(char *prefix, int seq, char *suffix)
+        std::string filenameBySeq(const char *prefix, int seq, const char *suffix)
         {
             return (boost::format("%s%06d%s") % prefix % seq % suffix).str();
         }
 
-        std::string filepathBySeq(char *prefix, int seq, char *suffix)
+        std::string filepathBySeq(const char *prefix, int seq, const char *suffix)
         {
             auto filename = (boost::format("%s%06d%s") % prefix % seq % suffix).str();
-            auto filepath = output_dir_.append(filename);
+            auto filepath = output_dir_;
+            filepath.append(filename);
+
             return filepath.string();
         }
 
